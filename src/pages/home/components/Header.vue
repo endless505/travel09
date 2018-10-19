350f12cba08bfd612f334f70a1248d6074c7c007
endless505 <you@example.com>
2018-10-19 20:49:16 +0800

    test

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="header_left">
+      <div class="iconfont">&#xe6bc;</div>
+    </div>
+    <div class="header_input">
+      <span class="iconfont">&#xe662;</span>
+      <input type="text" placeholder="请输入你想要去的地点">
+    </div>
+    <div class="header_right">
+      <span style="padding-left:5px;">成都</span>
+      <span class="iconfont">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+.header{
+  height: .88rem;
+  line-height: .88rem;
+  background: red;
+  display: flex;
+}
+.header_left{
+  float: left;
+  width: .60rem;
+}
+.header_input{
+  flex: 1;
+  background: #fff;
+  border-radius: 3px;
+  height: .64rem;
+  line-height: .64rem;
+  margin-top: .12rem;
+}
+.header_right{
+  float: right;
+}
+.iconfont{
+  margin-left: 5px;
+}
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wraper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper_img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data: function() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        }
+      },
+      swiperList: [
+        {
+          id: "1001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1809/91/836652a03cc5f802.jpg_890x330_628a9416.jpg"
+        },
+        {
+          id: "1002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1810/fb/030e309242deb002.png_890x330_e904912c.png"
+        },
+        {
+          id: "1003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1810/27/73c955aefe37302.jpg_890x330_6961ae90.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.swiper_img {
+  width: 100%;
+}
+.wraper >>> .swiper-pagination-bullet-active {
+  background: #fff !important;
+}
+</style>
